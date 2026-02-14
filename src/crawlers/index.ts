446fb1f0d2a3392be39cabaa6e40c9a6e3c33433
unknown <refra2n@naver.com>
2026-02-14 17:06:26 +0900

    fix: DDCT 양쪽(Railway+Oracle VM) 모두 실행하도록 복원
    DDCT 서버가 클라우드 IP를 간헐적으로 차단하므로
    Railway와 Oracle VM 양쪽에서 실행하여 성공률 향상

--- src/crawlers/index.ts
+++ src/crawlers/index.ts
@@ -18,6 +18,7 @@ import { BnmtCrawler } from './bnmt.js'
 import { IfpcCrawler } from './ifpc.js'
 import { PnctCrawler } from './pnct.js'
 import { BctCrawler } from './bct.js'
+import { DdctCrawler } from './ddct.js'
 
 function createCrawlers(): BaseCrawler[] {
   return [
@@ -36,6 +37,7 @@ function createCrawlers(): BaseCrawler[] {
     new IfpcCrawler(TERMINALS.IFPC),
     new PnctCrawler(TERMINALS.PNCT),
     new BctCrawler(TERMINALS.BCT),
+    new DdctCrawler(TERMINALS.DDCT),
   ]
 }
 
